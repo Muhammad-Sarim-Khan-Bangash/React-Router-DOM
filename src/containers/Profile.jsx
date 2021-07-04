5a0d76ec7596801e5ed03818d7b1eb02d69cb703
Muhammad Sarim Khan Bangash <75114586+Muhammad-Sarim-Khan-Bangash@users.noreply.github.com>
2021-07-05 01:15:05 +0500

    Add useLocation Hook

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const Profile = () => {
   const { username } = useParams();
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div>
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900  text-center">
         Profile
       </h1>
       <h2 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-red-900  text-center">
-        {username}
+        Hello <span className="text-gray-900">{username}</span>
+      </h2>
+      <h2 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-red-900  text-center">
+        My current Location is {location.pathname}
       </h2>
     </div>
   );
